d35327745214692f802765fe0f8fcb82cef83386
ArianSR29 <green-m-an@hotmail.com>
2018-05-04 14:50:26 -0500

    RESDE al 50%

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="assets/libs/fontawesome/css/fontawesome.css">
     <link rel="stylesheet" href="assets/libs/fontawesome/css/fa-solid.css">
     <link rel="stylesheet" href="assets/libs/fontawesome/css/fa-brands.css">
+    <link rel="stylesheet" href="assets/libs/mdl/material.css">
 </head>
 <body>
     <div class="main-section">
@@ -25,13 +26,76 @@
       <div class="c-main">
           <h1 class="robotoCR fs28 white">DISMINUYE <span class="green3A robotoCB">70%</span> TUS PAGOS DE AGUA, LUZ Y GAS</h1>
           <div class="c-banner">
-              <div class="c-img"><img src="" alt=""></div>
-              <div class="c-calc-form"></div>
+              <div class="c-img"><img src="assets/img/home-complete.svg" alt="home-complete"></div>
+              <div class="c-calc-form">
+                <h5 class="fs16 robotoB white">Nosotros te asesoramos para convertir tu Casa Verde.</h5>
+                <form action="">
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input class="mdl-textfield__input" type="text" id="tb_nombre" require>
+                        <label class="mdl-textfield__label" for="tb_nombre">Nombre</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input class="mdl-textfield__input" type="main" id="tb_main" require>
+                        <label class="mdl-textfield__label" for="tb_main">Correo electrónico</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input class="mdl-textfield__input" type="tel" id="tb_tel">
+                        <label class="mdl-textfield__label" for="tb_nombre">Teléfono (opcional)</label>
+                    </div>
+                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Quiero calcular mi ahorro</button>
+                </form>
+                <div class="c-message"><span class="fs16 robotoR green3A">Convierte tu Casa Verde, cambia productos de tu hogar por electrodomesticos de bajo consumo y más</span></div>
+                <div class="c-info-form">
+                    <h3 class="fs30 robotoCB green3A">¡Te ayudamos a calcular tu Ahorro!</h3>
+                    <form action="">
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input class="mdl-textfield__input" type="text" id="tb_nombre" require>
+                            <label class="mdl-textfield__label" for="tb_nombre">Nombre</label>
+                        </div>
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input class="mdl-textfield__input" type="main" id="tb_main" require>
+                            <label class="mdl-textfield__label" for="tb_main">Correo electrónico</label>
+                        </div>
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input class="mdl-textfield__input" type="tel" id="tb_tel">
+                            <label class="mdl-textfield__label" for="tb_nombre">Teléfono (opcional)</label>
+                        </div>
+                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent info-btn">Deseo más información</button>
+                    </form>
+                </div>
+              </div>
+          </div>
+      </div>
+      <div class="c-beneficios-main">
+          <img src="assets/img/beneficios-responsive.svg" alt="beneficios-responsive">
+          <div class="c-beneficios">
+            <div class="beneficio-item">
+                <img src="assets/img/asesoria.svg" alt="">
+                <div>
+                    <h6 class="fs22 robotoCR green56">Asesoría / Calidad</h6>
+                    <span class="fs14 robotoR gray70"> Optimizamos tu presupuesto, con ecotecnologías certificadas</span>
+                </div>
+            </div>
+            <div class="beneficio-item">
+                <img src="assets/img/entrega-inmediata.svg" alt="">
+                <div>
+                    <h6 class="fs22 robotoCR green56">Entrega e Instalación Inmediata</h6>
+                    <span class="fs14 robotoR gray70">En 8 días, tendrás tus ecotecnologías instaladas en tu hogar o negocio</span>
+                </div>
+            </div>
+            <div class="beneficio-item">
+                <img src="assets/img/paga-menos.svg" alt="">
+                <div>
+                    <h6 class="fs22 robotoCR green56">Paga Menos</h6>
+                    <span class="fs14 robotoR gray70">Ahorra en todos tus pagos mensuales</span>
+                </div>
+            </div>
           </div>
       </div>
     </div>
 
     <script src="assets/libs/jQuery/jQuery-3-3-1.min.js"></script>
+    <script src="assets/libs/mdl/material.js"></script>
     <script src="assets/js/data-send.js"></script>
 </body>
 </html>
