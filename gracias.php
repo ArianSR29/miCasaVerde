206ce69a484daa5dee48df980faecb86f097119d
israel.sanchez <jisrael@cvrm.com.mx>
2018-05-08 14:16:09 -0500

    Cambios pagina Agradecimiento

--- gracias.php
+++ gracias.php
@@ -6,9 +6,8 @@
 
     <link rel="shortcut icon" href="assets/img/favicon.svg" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/miCasaVerdeGracias.css">
-    <link rel="stylesheet" href="assets/libs/fontawesome/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/libs/fontawesome/css/fa-solid.css">
-    <link rel="stylesheet" href="assets/libs/fontawesome/css/fa-brands.css">
+        <link rel="stylesheet" href="assets/libs/fontawesome/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/libs/fontawesome/css/fa-regular.css">
     <link rel="stylesheet" href="assets/libs/mdl/material.css">
   </head>
   <body>
@@ -21,23 +20,23 @@
       <div class="c-img">
         <img src="assets/img/one-drive/agradecimiento/check-responsive.svg" alt="">
       </div>
-      <div class="c-gracias-text1">
+      <div class="c-gracias-text1 green67 robotoCB fs28">
         <span>¡Gracias por contactarnos José Antonio!</span>
       </div>
-      <div class="c-gracias-text2">
+      <div class="c-gracias-text2 gray70 fs14 robotoCR">
         <span>Hemos recibido su mensaje, y nos pondremos en contacto lo antes posible.</span>
       </div>
       <div class="c-button-gracias">
         <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent info-btn">Regresar</button>
       </div>
-      <div class="c-info">
-          <a class="info" href="http://www.gasticom.mx/quienes_somos.html" >Más información</a>
+      <div class="c-info fs14 robotoB">
+          <a class="info green66" href="http://www.gasticom.mx/quienes_somos.html" >Más información</a>
         </div>
     </div>
     <!--copyRight-->
-    <div class="c-copyright">
-      <img src="assets/img/copy.svg" alt="">
-       <span>2018 Mi Casa Verde</span>
+    <div class="c-copyright green66 fs14 robotoR">
+      <i class="far fa-copyright white"></i>
+       <span class="white">2018 Mi Casa Verde</span>
     </div>
     <script src="assets/libs/mdl/material.js"></script>
   </body>
