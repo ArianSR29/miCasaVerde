600dbb95229baa0beae1515ab30c9c028b472b36
ArianSR29 <green-m-an@hotmail.com>
2018-05-09 10:22:45 -0500

    inicio de insercción

--- assets/js/data-send.js
+++ assets/js/data-send.js
@@ -1,3 +1,31 @@
-$( document ).ready(function() {
-    console.log( "ready DS!" );
+$(document).on('ready',function(){       
+
+
+
+
+
+    $('#btn-ingresar').click(function(){
+        var url = "datos_login.php";
+        $.ajax({                        
+           type: "POST",                 
+           url: url,                     
+           data: $("#formulario").serialize(), 
+           success: function(data)             
+           {
+             $('#resp').html(data);               
+           }
+       });
+    });
+
+
+
+
+
+
+
+
+
+
+
 });
+console.log("Hola desde Data Send");
